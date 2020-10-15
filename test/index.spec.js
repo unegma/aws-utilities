7ee7df99c542027d2171136e2b421c4d3d732526
Timothy Coleman <timhc22@gmail.com>
2020-10-15 20:30:00 +0100

    Add invokeLambda function and tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const AWS_URL = 'https://example.com';
+const awsSDK = require('aws-sdk');
 const AWS_REGION = 'eu-west-2';
 const SLACK_ERROR_LOG = 'https://example.com';
 const chai = require('chai');
@@ -6,16 +6,26 @@ const expect = chai.expect;
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
-const nock = require('nock')
-const AWSUtilities = require('../lib/AWSUtilities');
 const AWSIntegrationError = require('../lib/errors/AWSIntegrationError');
 
+// These help:
+// https://stackoverflow.com/questions/26243647/sinon-stub-in-node-with-aws-sdk
+// https://stackoverflow.com/questions/61516053/sinon-stub-for-lambda-using-promises
 describe('AWS Utilities Test', () => {
-  beforeEach(function() {
+  let aws; let lambda; let AWSUtilities;
+  before(function() {
+    lambda = { invoke: sinon.stub().returnsThis(), promise: sinon.stub() };
+    aws = sinon.stub(awsSDK, 'Lambda').callsFake(() => lambda);
+    aws.returns(lambda);
+    AWSUtilities = require('../lib/AWSUtilities'); // uses the above stubbed version of aws
   });
 
-  afterEach(function() {
+  after(function() {
+    aws.restore();
   });
+
+  beforeEach(() => {
+  })
 
   it('should create an instance of an AWSIntegrationError', () => {
     const error = new AWSIntegrationError('Error');
@@ -37,13 +47,9 @@ describe('AWS Utilities Test', () => {
   });
 
   it('should invoke Lambda', async () => {
-    nock(AWS_URL)
-        .get(uri => uri.includes('example-path'))
-        .reply(200, { response: "aws-response" });
-
-    const awsUtilities = new AWSUtilities('12345');
-    const response = await awsUtilities.invokeLambda('/example-path/');
-    expect(response.response).to.equal('aws-response');
+    const awsUtilities = new AWSUtilities(AWS_REGION);
+    const response = await awsUtilities.invokeLambda('functionName');
+    expect(lambda.invoke).to.have.been.calledOnce;
   });
 
 
